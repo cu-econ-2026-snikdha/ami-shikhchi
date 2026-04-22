47a2d7f5f5350e2ae354bb301f2bda02df926a63
cu-econ-2026-snikdha <beautybarua.831@gmail.com>
2026-04-22 10:55:17 +0600

    Create day1 .py

---
+++ day1 .py
@@ -0,0 +1,7 @@
+# Day 1 - 21 April 2026
+# My data science journey starts today
+# Goal: CU Economics → NUS Singapore
+
+print("Hello, I am Maria from Chittagong")
+print("HSC 2027 Batch")
+print("Today I opened my GitHub khata")
