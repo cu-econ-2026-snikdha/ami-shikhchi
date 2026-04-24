8729d6f46003d096c5919f00dac0bc65931b1a03
cu-econ-2026-snikdha <beautybarua.831@gmail.com>
2026-04-24 14:41:13 +0600

    Day 4: learned for loop and list iteration

---
+++ day4.py
@@ -0,0 +1,12 @@
+# Day 4 - For Loop Magic 
+
+friends = ["Puja", "Borne", "Doyamoni", "Sadia"]
+hsc_subjects = ["Bangla", "Economics", "English", "ICT"]
+
+print("My friends list:")
+for friend in friends:
+    print("Hello", friend)
+
+print("\nMy subjects:")
+for subject in hsc_subjects:
+    print("I study", subject)
