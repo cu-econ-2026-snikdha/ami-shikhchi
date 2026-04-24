35cb09ebda0134183479d51940ed7953baeec2e7
cu-econ-2026-snikdha <beautybarua.831@gmail.com>
2026-04-24 12:31:47 +0600

    Day 3 : Learned List With Index

---
+++ day3.py
@@ -0,0 +1,13 @@
+# Day 3: My first List
+friends = ["Kishur", "Beauty", "Tushar", "Shatabdi"]
+hsc_subjects = ["Bangla", "English", "Economics" , "ICT"]
+dream_numbers = [17, 18, 19, 20]
+
+print("My best friend is")
+print(friends[0]) # 0 মানে প্রথম জন = Kishur 
+
+print("My favorite subject is")
+print(hsc_subjects[2]) # 2 মানে তৃতীয়টা = Economics 
+
+print("Next year I will be")
+print(dream_numbers[1]) # 1 মানে দ্বিতীয়টা = 18
