436aef01771984a61ae61a2a886f651b78b07024
cu-econ-2026-snikdha <beautybarua.831@gmail.com>
2026-05-02 14:37:47 +0600

    DAY 9 :  List and for loop practice

---
+++ day9.py
@@ -0,0 +1,9 @@
+# Day 9: List and for loop practice 
+colors = ["Red", "Blue", "Yellow", "Green"] 
+marks = [93, 94, 96, 92, 97] 
+
+print("I got", marks, "marks in the PSC exam")
+
+print("My favorite colors are:") 
+for color in colors: 
+    print(color)
