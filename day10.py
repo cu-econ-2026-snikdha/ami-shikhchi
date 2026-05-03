449611a9046bf6af08c4b3fda2828461d5fcd64c
cu-econ-2026-snikdha <beautybarua.831@gmail.com>
2026-05-03 14:21:41 +0600

    Day 10 : Practice function to display sentences from a list

---
+++ day10.py
@@ -0,0 +1,9 @@
+# Day 10 : Practice function + for loop  
+
+def show_text(sentence):
+    for sen in sentence:
+        print(sen)
+
+book_fair = ["A book fair is a place which displayed various kinds of book"]
+
+show_text(book_fair) 
