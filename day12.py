45b3cbf9b2f43775cd7b6d43ed46887c10ba6315
cu-econ-2026-snikdha <beautybarua.831@gmail.com>
2026-05-08 18:23:10 +0600

    Day 12 : Calculate total and average marks

---
+++ day12.py
@@ -0,0 +1,7 @@
+bangla = int(input("Bangla mark: "))
+english = int(input("English mark: "))
+total = bangla + english
+average = total / 2
+
+print("Total mark:", total)
+print("Average mark:", average) 
