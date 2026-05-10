90873b72e7a14f9e8725bae84af518a5f191f0f2
cu-econ-2026-snikdha <beautybarua.831@gmail.com>
2026-05-10 17:13:16 +0600

    Day 13 : Monthly tuition payment calculation
    Calculate total and average monthly tuition payments.

---
+++ day13.py
@@ -0,0 +1,11 @@
+# Day 13 : My monthly tuition payment calculation with variable 
+
+sima = 300 
+avoy = 300 
+probal= 500
+disa = 300 
+total = sima + avoy + probal + disa 
+average = total / 4
+
+print("Monthly total income", total, "tk")
+print("Average monthly income", average,"tk") 
