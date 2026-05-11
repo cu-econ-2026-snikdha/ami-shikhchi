b6ad8a9003f352c12d08b7bc994e73065058e06b
cu-econ-2026-snikdha <beautybarua.831@gmail.com>
2026-05-11 11:52:59 +0600

    Add monthly income calculation script

---
+++ day14.py
@@ -0,0 +1,8 @@
+# Day 14 : Monthly income calculation with list
+
+income = [300, 500, 300, 500]
+total = sum(income)
+
+print(f"Total revenue {total} tk") # মোট আয়: 1600 tk
+print(f"Average revenue {total/len(income)} tk") # গড় আয়: 400.0 tk
+print(f"Marginal revenue {income[-1]} tk") # শেষ পেমেন্ট: 500 tk
