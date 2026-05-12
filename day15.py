7a6121e6c06af581dd09d8abe768bdce406725d1
cu-econ-2026-snikdha <beautybarua.831@gmail.com>
2026-05-12 22:37:53 +0600

    Day 15: Average calculation and pass/fail logic
    Calculate average of numbers and determine pass/fail status based on mark.

---
+++ day15.py
@@ -0,0 +1,11 @@
+numbers = [2, 4, 6, 8, 10, 12]
+total = 0 
+for N in numbers:
+  total = total + N
+print(f"Average: {total/len(numbers)}") 
+
+mark = 29
+if mark <= 33: 
+    print("You will fail")
+else:
+    print("You will pass") 
